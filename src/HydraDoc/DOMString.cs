945d5296205e2ccef28ce65c1d1c60dc6a088298
jherink <jacobherink@gmail.com>
2017-04-17 17:10:42 -0500

    Add files via upload

---
+++ src/HydraDoc/DOMString.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using HydraDoc.Elements;
+using HydraDoc.Elements.Interface;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HydraDoc
+{
+    public struct DOMString : IElement, ICloneable
+    {
+        public ClassList ClassList { get; set; }
+
+        public StyleList StyleList { get; set; }
+
+        public IDictionary<string, string> Attributes { get; private set; }
+
+        public string ID { get; set; }
+
+        public string Tag { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get { return Render(); } }
+
+        public ICollection<IElement> Elements { get; private set; }
+
+        public string Render()
+        {
+            var builder = new StringBuilder();
+            foreach (var element in Elements)
+            {
+                builder.Append( element.Render() );
+            }
+            return builder.ToString();
+        }
+
+        public DOMString( string content ) : this( new PlainText( content ) )
+        {
+        }
+
+        public DOMString( int value ) : this( value.ToString() ) { }
+
+        public DOMString( double value ) : this( value.ToString() ) { }
+
+        public DOMString( params IElement[] content )
+        {
+            ID = string.Empty;
+            Tag = string.Empty;
+            Title = string.Empty;
+            ClassList = new ClassList();
+            StyleList = new StyleList();
+            Attributes = new Dictionary<string, string>();
+            Elements = new List<IElement>();
+            foreach (var item in content)
+            {
+                Elements.Add( item );
+            }
+        }
+
+        //public static DOMString Create()
+        //{
+        //    var str = new DOMString();
+        //    str.ID = str.Tag = str.Title = string.Empty;
+        //    str.ClassList = new ClassList();
+        //    str.Elements = new List<IElement>();
+        //    return str;
+        //}
+
+        public void Append( string content )
+        {
+            AppendElement( new PlainText( content ) );
+        }
+
+        public void AppendElement( IElement content )
+        {
+            if (Elements == null) Elements = new List<IElement>();
+            Elements.Add( content );
+        }
+
+
+        #region Implicit Operators With string
+
+        public static implicit operator DOMString( string str )
+        {
+            return new DOMString( str );
+        }
+
+        public static implicit operator string( DOMString str )
+        {
+            return str.Render();
+
+            // For whatever reason I got an Overflow exception
+            // when checkin if str was null.  Do this and hope for the 
+            // best...
+            //return str != default( DOMString ) ? str.Render() : 
+            //                                     default( string );
+        }
+
+        public static implicit operator DOMString( int value )
+        {
+            return new DOMString( value );
+        }
+
+        public static implicit operator DOMString( double value )
+        {
+            return new DOMString( value );
+        }
+
+
+        #endregion
+
+        #region Operator Overloads
+
+        public static DOMString operator +( DOMString left, DOMString right )
+        {
+            return new DOMString( left.Text + right.Text );
+        }
+
+        public static DOMString operator +( DOMString left, IElement right )
+        {
+            var result = (DOMString)left.Clone();
+            result.Elements.Add( right );
+            return result;
+        }
+
+        public static bool operator ==( DOMString left, string right )
+        {
+            return left.Render() == right;
+        }
+
+        public static bool operator !=( DOMString left, string right )
+        {
+            return left.Render() != right;
+        }
+
+        public static bool operator ==( string left, DOMString right )
+        {
+            return left == right.Render();
+        }
+
+        public static bool operator !=( string left, DOMString right )
+        {
+            return left != right.Render();
+        }
+
+        #endregion
+
+        #region object overrides
+
+        public override string ToString()
+        {
+            return Text;
+        }
+
+        public override bool Equals( object obj )
+        {
+            try
+            {
+                var o = (DOMString)obj;
+                if (o != null)
+                {
+                    return o.Text.Equals( Text );
+                }
+            }
+            catch (InvalidCastException) { }
+
+            var o2 = obj as string;
+            if (o2 != null)
+            {
+                return o2.Equals( Text );
+            }
+
+            if (obj != null)
+            {
+                var o3 = obj.ToString();
+                return o3.Equals( Text );
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Text.GetHashCode();
+        }
+
+        #endregion
+
+        #region ICloneable Implementation
+
+        public object Clone()
+        {
+            // this can happen if initialized from default struct constructor. :(
+            if (Elements == null)
+            {
+                Elements = new List<IElement>();
+            }
+            if (ClassList == null)
+            {
+                ClassList = new ClassList();
+            }
+
+            var domString = (DOMString)MemberwiseClone();
+            domString.ID = string.Empty;
+            domString.Elements = new List<IElement>();
+            domString.ClassList = new ClassList();
+            foreach (var element in Elements)
+            {
+                domString.Elements.Add( element.Clone() as IElement );
+            }
+            foreach (var cls in ClassList)
+            {
+                domString.ClassList.Add( cls );
+            }
+            return domString;
+        }
+
+        #endregion
+    }
+
+}
---
+++ src/HydraDoc/HydraDocument.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HydraDoc.CSS;
+using HydraDoc.Elements;
+using HydraDoc.Elements.Interface;
+using System.Globalization;
+using System.Xml;
+
+namespace HydraDoc
+{
+    public class HydraDocument
+    {
+        public readonly IHtmlElement Page;
+
+        public IHeadElement Head
+        {
+            get { return Page.Head; }
+        }
+
+        public IBodyElement Body
+        {
+            get
+            {
+                return Page.Body;
+            }
+        }
+
+        private IStyleElement CustomStyles;
+
+        public string Language { get; set; }
+
+        public HydraDocument()
+        {
+            Page = new Html();
+            Page.Children.Add( new Head() ); // add a head
+            Page.Children.Add( new Body() ); // and a body to the page.
+            Language = CultureInfo.CurrentCulture.TwoLetterISOLanguageName; // set to current culture.
+
+            // add mobile viewport meta tag.
+            Head.Metas.Add( ElementFactory.CreateMeta( "viewport", "width=device-width, initial-scale=1" ) );
+
+            // add w3 resource.
+            Head.Styles.Add( ElementFactory.CreateStyleFromResource( "w3" ) );
+            // add graph resource.
+            Head.Styles.Add( ElementFactory.CreateStyleFromResource( "graph" ) );
+
+            // add a custom styles style sheet for user defined rules.
+            CustomStyles = ElementFactory.CreateStyle( new StyleSheet() );
+            Head.Styles.Add( CustomStyles );
+
+            //var assembly = typeof( StyleSheet ).Assembly;
+            //var name = "HydraDoc.CSS.Themes.w3.css";
+            //using (var stream = assembly.GetManifestResourceStream( name ))
+            //{
+            //    using (var reader = new StreamReader( stream ))
+            //    {
+            //        var parser = new Parser();
+            //        Head.Styles.Add( ElementFactory.CreateStyle( parser.Parse( reader.ReadToEnd() ) ) );
+            //    }
+            //}
+
+            // load w3 style.
+            //var loader = new CssThemeLoader();
+            //loader.LoadThemeResource( "w3" );
+            //Head.Styles.Add( ElementFactory.CreateStyle(loader.StyleSheet) );
+        }
+
+        public IDivElement AddBodyContainer()
+        {
+            var div = ElementFactory.CreateDiv( true );
+            Body.Children.Add( div );
+            return div;
+        }
+
+        /// <summary>
+        /// Add an element to the body of the document.
+        /// </summary>
+        /// <param name="element"></param>
+        public void Add( IElement element )
+        {
+            Body.Children.Add( element );
+        }
+
+        public bool Remove( IElement element )
+        {
+            return Body.Children.Remove( element );
+        }
+
+        public void AddStyle( IStyleElement style )
+        {
+            Head.Styles.Add( style );
+        }
+
+        public void AddMeta( IMetaElement meta )
+        {
+            Head.Metas.Add( meta );
+        }
+
+        /// <summary>
+        /// Add a CSS rule.
+        /// </summary>
+        /// <param name="css">The CSS rule</param>
+        public void AddStyleRule( string css )
+        {
+            var parser = new Parser();
+            var sheet = parser.Parse( css );
+            foreach (var rule in sheet.Rules)
+            {
+                CustomStyles.StyleSheet.Rules.Add( rule );
+            }
+        }
+
+        public IElement Find( string id )
+        {
+            return Body.FindById( id );
+        }
+
+        public string Render()
+        {
+            var allNodes = Page.GetAllNodes();
+            var nodes = allNodes.Select( t => t.Tag ).Distinct();
+            var classes = new List<string>();
+            foreach (var node in allNodes)
+            {
+                classes.AddRange( node.ClassList.Where( t => true ) );
+            }
+            var filteredClasses = classes.Distinct();
+
+            var builder = new StringBuilder();
+            builder.AppendLine( $"<html lang=\"{Language}\">" );
+            builder.AppendLine( Head.Render( nodes, filteredClasses ) );
+            builder.AppendLine( Body.Render() );
+            builder.AppendLine( "</html>" );
+            return builder.ToString();
+        }
+
+        public void Save( string path )
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml( Render() );
+
+            var settings = new XmlWriterSettings
+            {
+                Indent = true,
+                IndentChars = "\t"
+            };
+
+            var writer = XmlWriter.Create( path, settings );
+            xml.Save( writer );
+        }
+    }
+}
